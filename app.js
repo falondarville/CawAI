ac60c07e4f10fad0d60993cb99e9f7f419c1040f
Falon Darville <falondarville@gmail.com>
2018-06-26 13:33:17 -0700

    add passport config to app.js

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 require('dotenv').config()
-var cors = require('cors');
-var nodemon = require('nodemon');
-var mysql = require('mysql');
+const cors = require('cors');
+const nodemon = require('nodemon');
+const mysql = require('mysql');
 const bcrypt = require('bcrypt');
+const passport = require('passport')
+  	, LocalStrategy = require('passport-local').Strategy;
 const db = require('./models');
 
-var watsonRouter = require('./routes/watson')
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var addUserRouter = require('./routes/addUser');
+const watsonRouter = require('./routes/watson')
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const addUserRouter = require('./routes/addUser');
 
-var app = express();
+const app = express();
 
-var port = process.env.port || 3000;
+const port = process.env.port || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,7 +31,55 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
+app.use(session({
+  secret: "double macchiato STAT",
+  resave: true,
+  name: "oatmealRaisin",
+  proxy: true,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// passport initialize and serialize
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+})
+
+passport.deserializeUser(function(id, done){
+  db.Users.findById(id).then(function(user){
+    if(user){
+      done(null, user.get());
+    } else {
+      done(user.errors, null);
+    }
+  })
+})
+
+// passport log in authentication
+passport.use(new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password'
+},
+  function(username, password, done) {
+    db.Users.findOne({ 
+      where:{ email: username }
+      }).then(function(user){
+        if (user ==null) {
+          return done(null, false, {message: 'Incorrect credentials'})
+        }
+        bcrypt.compare(password, user.password, function(err, res){
+          if(res) {
+              done(null, user)
+          } else {
+              done(null, false, { message: 'Incorrect password.' })
+          }
+      });
+    })
+   }
+))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
