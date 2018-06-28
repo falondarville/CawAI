5b35ab837e15baaa07f1a8f83fba8c5e894fd10d
Falon Darville <falondarville@gmail.com>
2018-06-27 18:29:19 -0700

    fix styling on svg

--- client/src/components/loggedin.js
+++ client/src/components/loggedin.js
@@ -5,14 +5,27 @@ import './loggedin.css';
 export default class LoggedIn extends Component {
 
 	// axios get request to get past searches associated with an account
-	
 
-	constructor(){
-		super();
-		this.state: {
+	// constructor(){
+	// 	super();
+	// 	this.state: {
+	// 		date: '',
+	// 		search: '',
+	// 		results: ''
+	// 	}
+	// }
 
-		}
-	}
+	// var self = this;
+
+	// axios.post('/authuser').then(function(data){
+	// 	const userData = data.data
+
+	// 	self.setState({
+	// 		user: {
+
+	// 		}
+	// 	})
+	// })
 
 	render(){
 		return (
