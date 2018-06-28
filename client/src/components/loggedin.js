e41fb45b02f6524ede202932afe868c207ba0975
Falon Darville <falondarville@gmail.com>
2018-06-28 15:52:18 -0700

    add check to see if user is logged in

--- client/src/components/loggedin.js
+++ client/src/components/loggedin.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // will need to add Redirect once I integrate checking if the user is logged in or not. If not, the user will be redirected to the homepage and cannot manually access the loggedin page
-import { Link } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 // import axios from 'axios';
 import './loggedin.css';
 
@@ -10,44 +10,56 @@ export default class LoggedIn extends Component {
 		this.props.history.push('/analysis');
 	}
 
-	// this page should only be accessible when a user is logged in
+	// 1. this page should only be accessible when a user is logged in
 	// use redirectToLogin method that you used with USUME
 	// https://github.com/falondarville/usume/blob/master/client/src/components/loggedin.jsx
-	// axios get request to get past searches associated with an account
-	// add functionality for users to be able to clear past results 
 
-	// constructor(){
-	// 	super();
-	// 	this.state: {
-	// 		date: '',
-	// 		search: '',
-	// 		results: ''
-	// 	}
-	// }
+	// 2. axios get request to get past searches associated with an account
+	// 3. Additional functionality: users can clear past results 
 
-	// var self = this;
+	constructor() {
+		super();
+		this.state = {
+			search: '',
+			results: ''
+		},
+		redirectToLogin: false
+	}
 
-	// axios.post('/authuser').then(function(data){
-	// 	const userData = data.data
+	var self = this;
 
-	// 	self.setState({
-	// 		user: {
+	axios.post('/authuser').then(function(data){
+		const userData = data.data
 
-	// 		}
-	// 	})
-	// })
+		// I want to display date and snippet from the database information on panels. Once you click on a panel, I want user to be taken to a detail page. Note that multiple searches and results will need to be stored in state (as an array?)
+		self.setState({
+			search: userData.search, 
+			results: userData.search
+		})
+		.catch(function(error){
+			self.setState({ redirectToLogin: true });
+		})
+	})
 
 	render(){
+
+		const { redirectToLogin } = this.state;
+
+		if(redirectToLogin) {
+			return <Redirect to={{ pathname: '/login' }} />
+		} else {
+
 		return (
 			<div>
 				<Link className="float-left ml-5 mt-3" to="/">Home</Link>
 				<Link className="float-right mr-5 mt-3" to="/logout">Log Out</Link>
 				<div className="container text-center">
 					<button className="btn btn-start mt-5 mb-3" onClick={this.handleClick}>New Search</button>
-				{/*if statement to either display the following title or display saved searches*/}
+					{/*if statement to either display the following title or display saved searches*/}
 					<h2>You have no saved searches yet.</h2>
 				</div>
 			</div>
 		)
+	}
 	}
 }
---
+++ routes/authUser.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const passport = require('passport')
+const cors = require('cors')
+  	, LocalStrategy = require('passport-local').Strategy;
+const router = express.Router();
+const db = require('./../models');
+const app = express();
+
+app.use(cors());
+
+// check 
+router.post('/authuser', function(request, response){
+	if(request.user) {
+		// query the second table
+		db.UserData.find({
+			where: {userId: request.user.id}
+		}).then(function(data){
+			response.json({
+				search: data.search,
+				results: data.results
+			})
+		})
+	} else {
+		response.status(401);
+		response.json({message: "not logged in"})
+	}
+})
+
+module.exports = router;
